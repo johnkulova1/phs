effb3c2ee6024cf459309d04c1e26c39cde2c3ec
John Ngichabe <johnngichabe@MacBook-Pro.local>
2015-03-03 16:58:06 +0300

    Third commit

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -34,14 +34,61 @@
 		              'id'          => 'searchucr',
 		              'value'       => 'Search for UCR here',
 		              'maxlength'   => '100',
-		              'size'        => '50'
+		              'size'        => '50',
+		              'style'		=> 'height:25px; padding-left:10px;'
 		            );
 			echo form_input($data);
 			?>
 		</span>
+		<span id="approved" class="applicationstatus" style="margin-left:20px;">
+			<a style="color:#71c36d; font-family:verdana;" href="#">Approved - 23</a>
+		</span>
+		<span id="pending" class="applicationstatus" style="margin-left:20px;">
+			<a style="color:#746c6c; font-family:verdana;" href="#">Pending - 10</a>
+		</span>
+		<span id="rejected" class="applicationstatus" style="margin-left:20px;">
+			<a style="color:#ff0000; font-family:verdana;" href="#">Rejected - 5</a>
+		</span>
 	</div>
-	<div>
-
+	<div style="margin-top:10px; padding-left:10px; padding-right:10px;">
+		<table width="100%" border="1" class="dashboardtable">
+		  <tr style="background-color:#868686; color: #fff; height: 50px; text-align:center;">
+		    <td>Reference No.</td>
+		    <td>Approval status</td>
+		    <td>UCR No.</td>
+		    <td>Consign Type</td>
+		    <td>Process Name</td>
+		    <td>Date submitted</td>
+		    <td>Approval history</td>
+		  </tr>
+		  <tr>
+		    <td>MD201300HCDAEP0100000295</td>
+		    <td>Approved pending eDoc Fee</td>
+		    <td>0</td>
+		    <td>Master Approval</td>
+		    <td>EPA Permit and Consumer Consignment</td>
+		    <td>06-06-2013 11:11:37</td>
+		    <td><a href="#">view</a></td>
+		  </tr>
+		  <tr>
+		    <td>CD201300HCDAEP0100000295</td>
+		    <td>Rejected</td>
+		    <td>UCR201300000201</td>
+		    <td>Consignment</td>
+		    <td>PVOC DOC TEST</td>
+		    <td>06-06-2013 11:11:37</td>
+		    <td><a href="#">view</a></td>
+		  </tr>
+		  <tr>
+		    <td>MD201300HCDAEP0100000295</td>
+		    <td>Approved pending eDoc Fee</td>
+		    <td>0</td>
+		    <td>Master Approval</td>
+		    <td>EPA Permit and Consumer Consignment</td>
+		    <td>06-06-2013 11:11:37</td>
+		    <td><a href="#">view</a></td>
+		  </tr>
+		</table>
 	</div>
 </body>
 </html>
